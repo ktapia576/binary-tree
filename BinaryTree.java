d8616aab053cf04b2fb87783db4fc7ec6856dd3e
ktapia576 <ktapia576@hotmail.com>
2024-07-07 11:32:46 -0400

    Added inorder traversal and add recursive methods

--- BinaryTree.java
+++ BinaryTree.java
@@ -17,6 +17,11 @@ public class BinaryTree {
      public static void main(String[] args) {
           Node root = new Node(10);
 
+          addRecursive(root, 9);
+          inOrderPrint(root);
+
+          //spacer for inOrder print
+          System.out.println();
           System.out.println("Test");
           System.out.println(root.value);
      }
@@ -26,9 +31,32 @@ public class BinaryTree {
                return new Node(value);
           }
 
-
-
+          if(value < current.val){
+               current.left = addRecursive(current.left, value);
+          }
+          else if(value > current.val){
+               current.right = addRecursive(current.right, value);
+          }
+          else {
+               //value exists
+               return current;
+          }
 
           return current;
+     }
+
+     /**
+      * Prints binary tree in order. Passing the root as initial argument is recommended.
+      *
+      * @param current holds the current node in the traversal
+      */
+     private static void inOrderPrint(Node current){
+          if(current == null){
+               return;
+          }
+
+          inOrderPrint(current.left);
+          System.out.print(current.val + " ");
+          inOrderPrint(current.right);
      }
 }
